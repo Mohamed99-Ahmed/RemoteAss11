a628fa8498d9bc8e8dca7646489106ba87e5b025
Mohamed99-Ahmed <mohamed202029999@gmail.com>
2024-10-04 09:24:18 +0300

    Upplod V1 to Server

---
+++ js/main.js
@@ -0,0 +1,111 @@
+// start sections  Variables
+const loginSection = document.querySelector("#signin");
+const signUpSection = document.querySelector("#signup");
+const homeSection = document.querySelector("#home");
+// start login Section  variables
+const  loginEmail = document.querySelector("#emailLogin");
+const  loginPassword = document.querySelector("#passwordLogin");
+const  loginBtn = document.querySelector("#loginBtn");
+const signinError = document.querySelector("#signinError")
+const signinEmaiError = document.querySelector("#signinEmaiError")
+const signUpLink = document.querySelector("#signUpLink");
+// start home Section variables
+const homeBtn = document.querySelector("#homeBtn");
+const logoutBtn = document.querySelector("#logoutBtn");
+// Start sign-up Section variables
+const signUpName = document.querySelector("#nameSignUp");
+const signUpEmail = document.querySelector("#emailSignUp");
+const signUpPassword = document.querySelector("#passwordSignUp");
+const signUpBtn = document.querySelector("#signUpBtn");
+const signUpError = document.querySelector("#signUpError");
+const emailError = document.querySelector("#emailError");
+const successSignUp = document.querySelector("#successSignUp");
+const signInLink = document.querySelector("#signInLink");
+// start global variables
+let valEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+let valPassword = /^[@][A-Z][A-Za-z0-9]{4,}$/;
+let users ;
+if(localStorage.getItem("users")){
+    users = JSON.parse(localStorage.getItem("users"));
+}else{
+    users = [];
+}
+
+// start functions
+
+// Add user function to user array and localStorage
+function addUser(){
+    let user = {
+        name:signUpName.value,
+        email:signUpEmail.value,
+        password:signUpPassword.value,
+    }
+    users.push(user);
+    localStorage.setItem("users",JSON.stringify(users));
+}
+// Sign up functions and Add user
+function validateSignUp(){
+    if(signUpName.value === "" || signUpEmail.value === "" || signUpPassword.value === ""){
+        signUpError.classList.remove("d-none"); //Display Eorror (All inputs must be requied)
+    }else{
+        // loop in userus and if an
+        if(users.some(user => user.email === signUpEmail.value) || !valEmail.test(signUpEmail.value)){
+            emailError.classList.remove("d-none");
+            signUpError.classList.add("d-none");
+        }else if(!valPassword.test(signUpPassword.value)){
+            passwordError.classList.remove("d-none");
+            signUpError.classList.add("d-none");
+            emailError.classList.add("d-none");
+        }
+        else{
+            addUser();
+            successSignUp.classList.remove("d-none");
+            signUpError.classList.add("d-none");
+            emailError.classList.add("d-none");
+            passwordError.classList.add("d-none");
+        }
+    }
+}
+
+
+// start Login functions
+
+function validateSignIn() {
+    if (loginEmail.value == "" || loginPassword.value == "") {
+        signinError.classList.remove("d-none");
+    } 
+    else {
+        let userFound = false;
+        for (let i = 0; i < users.length; i++) {
+            if (loginEmail.value == users[i].email && loginPassword.value == users[i].password) {
+                signinError.classList.add("d-none");
+                signinEmaiError.classList.add("d-none");
+                loginSection.classList.add("d-none");
+                homeSection.classList.remove("d-none");
+                homeSection.querySelector("article .box h1 span").innerHTML = users[i].name;
+                userFound = true;
+                break;
+            }
+        }
+        if (!userFound) {
+            signinEmaiError.classList.remove("d-none");
+            signinError.classList.add("d-none");
+        }
+    }
+}
+
+loginBtn.addEventListener("click", validateSignIn);
+signUpLink.addEventListener("click",()=>{
+    loginSection.classList.add("d-none");
+    signUpSection.classList.remove("d-none");
+})
+
+logoutBtn.addEventListener("click",  () => {
+    homeSection.classList.add("d-none");
+    loginSection.classList.remove("d-none");
+});
+signUpBtn.addEventListener("click",validateSignUp);
+signInLink.addEventListener("click",()=>{
+    loginSection.classList.remove("d-none");
+    signUpSection.classList.add("d-none");
+});
